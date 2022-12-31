a9bfe3558fca8e0ecd5a9368df0995b6838ba129
Mudassir <mudassarmr988@gmail.com>
2022-12-31 14:13:06 +0530

    Commit 2

--- src/test/java/DevTest.java
+++ src/test/java/DevTest.java
@@ -1,5 +1,8 @@
 public class DevTest {
     public static void main(String[] args) {
         System.out.println("this is dev class from test");
+        System.out.println("commit 2 ");
+        //  new linne is added here
+
     }
 }
