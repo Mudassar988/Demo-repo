35dc44d2e68962a1d93f754a572fc1c07c0c7ac4
Mudassir <mudassarmr988@gmail.com>
2023-02-16 16:29:47 +0530

    yes changes

--- Newclass.java
+++ Newclass.java
@@ -1,5 +1,5 @@
 public class Newclass {
     public static void main(String[] args) {
-        System.out.println("commit 1 form demo repo README File ");
+        System.out.println("yes commit 1 form demo repo README File ");
     }
 }
